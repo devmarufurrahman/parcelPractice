ac9c5bc7d319df77b3bbe725a82d7d4c52a09d24
marufur-rahman <mdmarufur5682@gmail.com>
2023-03-29 11:33:00 +0600

    parcel practice

---
+++ dist/index.95c31743.js
@@ -0,0 +1,2 @@
+let a=20,b=30;const sum=a+b;console.log(sum);const arr=[1,2,3,4,5,6,7];console.log(arr.at(-1));
+//# sourceMappingURL=index.95c31743.js.map
---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+let a = 20;
+let b = 30;
+const sum = a + b;
+console.log(sum);
+
+const arr = [1, 2, 3, 4, 5, 6, 7];
+
+console.log(arr.at(-1));
